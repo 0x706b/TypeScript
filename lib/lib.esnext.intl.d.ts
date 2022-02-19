62a03d68391883259d4421e77957a7c89e1b72dc
Peter Krol <peter@peterkrol.com>
2022-02-19 09:42:22 -0500

    artifact: tsplus-20220219

--- lib/lib.esnext.intl.d.ts
+++ lib/lib.esnext.intl.d.ts
@@ -19,14 +19,8 @@ and limitations under the License.
 
 
 declare namespace Intl {
-    type NumberFormatPartTypes = "compact" | "currency" | "decimal" | "exponentInteger" | "exponentMinusSign" | "exponentSeparator" | "fraction" | "group" | "infinity" | "integer" | "literal" | "minusSign" | "nan" | "plusSign" | "percentSign" | "unit" | "unknown";
 
-    interface NumberFormatPart {
-        type: NumberFormatPartTypes;
-        value: string;
-    }
-
-    interface NumberFormat {
-        formatToParts(number?: number | bigint): NumberFormatPart[];
-    }
+   interface DateTimeFormat {
+      formatRange(startName: Date, endNumber: Date): string;
+   }
 }
