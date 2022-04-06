41bf8b6b34fd8ffbb8d86a18c2a93b31ed0491e0
Peter Krol <peter@peterkrol.com>
2022-04-05 21:11:54 -0400

    fix: avoid potentially recursive checking in isTsPlusMacroCall

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -1484,13 +1484,17 @@ namespace ts {
             if (!isCallExpression(node)) {
                 return false;
             }
-            const type = getTypeOfNode(node.expression);
-            if (type && type.symbol && type.symbol.declarations) {
-                for (const declaration of type.symbol.declarations) {
-                    if (getAllJSDocTags(declaration, (_): _ is JSDocTag => _.tagName.escapedText === "tsplus" && _.comment === `macro ${macro}`).length > 0) {
-                        return true;
-                    }
-                }
+            if (isIdentifier(node.expression) && node.expression.escapedText === "identity" as __String) {
+                debugger;
+            }
+            const links = getNodeLinks(node);
+            if (links.resolvedSignature &&
+                links.resolvedSignature.declaration &&
+                getAllJSDocTags(
+                    links.resolvedSignature.declaration,
+                    (_): _ is JSDocTag => _.tagName.escapedText === "tsplus" && _.comment === `macro ${macro}`).length > 0
+                ) {
+                return true;
             }
             return false;
         }
