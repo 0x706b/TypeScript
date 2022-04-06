6dbfebfe12fd33e0191d7c227ac949afb9977225
Peter Krol <peter@peterkrol.com>
2022-04-05 20:23:47 -0400

    artifact: tsplus-20220406

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -1484,9 +1484,9 @@ namespace ts {
             if (!isCallExpression(node)) {
                 return false;
             }
-            const type = getTypeOfNode(node.expression);
-            if (type && type.symbol && type.symbol.declarations) {
-                for (const declaration of type.symbol.declarations) {
+            const links = getNodeLinks(node.expression);
+            if (links.resolvedSymbol && links.resolvedSymbol.declarations) {
+                for (const declaration of links.resolvedSymbol.declarations) {
                     if (getAllJSDocTags(declaration, (_): _ is JSDocTag => _.tagName.escapedText === "tsplus" && _.comment === `macro ${macro}`).length > 0) {
                         return true;
                     }
